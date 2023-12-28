13a5aafc47911efd06d8a42ab4d28213b47a099f
Adel Kandi <adelkandi68@gmail.com>
2023-12-28 19:20:50 +0100

    Update

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import discord
 import os
 from dotenv import load_dotenv
 
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
 load_dotenv()
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
 intents = discord.Intents.default()
 intents.message_content = True
 Bot = discord.Client(intents=intents)   # Connect the bot to discord server
@@ -11,12 +12,14 @@ Bot = discord.Client(intents=intents)   # Connect the bot to discord server
 @Bot.event
 async def on_ready():
     print("we have logged in as {0.user}".format(Bot))
+    
 
 @Bot.event
 async def on_message(message):
     if message.author == Bot.user:
         return
-    
+    if message.content.startswith("$News"):
+        await message.channel.send("Hello there im here for youi have some News") 
 
 Bot.run(DISCORD_TOKEN)
 
--- src/commands/command.py
+++ src/commands/command.py
@@ -1,15 +1,53 @@
 from discord.ext import commands, tasks
+from discord import Embed
 import requests
+import main
+import json
 from utils import get_last_news, send_news
 
-async def get_last_news():
+async def get_latest_news():
     url = "https://hacker-news.firebaseio.com/v0/topstories.json"
-    response = requests.get(url)
+    response = requests.get(url)    
+    storys_ids = response.json()[:5] 
+    news = ""
+    for story_id in storys_ids:
+        story_url = f"https://hacker-news.firebaseio.com/v0/item/{story_id}.json"
+        story_data = requests.get(story_url).json
 
-    return 
+    return {
+        "title":"Breaking News",
+        "description":"Something entersting happen",
+        "image_url":"https://example.com/news_image.jpg",
+        "url": "https://example.com/full_article",
+    }
+
 
 
 async def send_news():
+    channelId = input("Give us the ChannelId where you want the bot to work: ")
+    print("The bot {0.user}".format(main.Bot),f" will work on channelID {channelId}")
+    # ChannelID for my server is: 1189989133674885322 
+    channel = main.Bot.get_channel(channelId)
+    
+    # Fetch to the letest news
+    latest_news = get_last_news() 
+
+    # Creat rich embed for the news
+    embed = Embed(
+        title = latest_news["title"],
+        description= latest_news["description"],
+        url = latest_news["url"],
+        color = 0x3498db
+    )
+    embed.set_thumbnail(url = latest_news["image_url"])
+
+    message = await channel.send(embed=embed) # Post the News on the channel
+
+    # Add imojis :
+    await message.add_reaction('👍')
+    await message.add_reaction('👎')
+    
+
     return 
 
 
